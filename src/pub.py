f2087ebe7d97069a1bc3fd42d22604971f45fd33
ScottDrysdale86 <sdrysdale86@pm.me>
2022-06-29 21:00:33 +0100

    added sufficient funds. Changed testing layout self.

--- src/pub.py
+++ src/pub.py
@@ -6,25 +6,23 @@ class Pub:
         self.drinks = {"Guinness": 5, "Corona": 1}
 
     def age_check(self, customer):
-        if customer.age >= 18:
-            return True
-        else:
-            return False
+        return customer.age >= 18
 
     def increase_till(self, amount):
         self.till += amount
 
     def drunkness_check(self, customer):
-        if customer.drunkness <= 10:
-            return True
-        else:
-            return False
+        return customer.drunkness <= 10
+
+    def sufficient_funds(self, customer, drink):
+        return customer.wallet >= drink.price
 
     def sell_drink(self, drink, customer):
         if (
             self.age_check(customer)
             and self.drunkness_check(customer)
             and self.check_quantity(drink.name) > 0
+            and self.sufficient_funds(customer, drink)
         ):
             customer.reduce_wallet(drink.price)
             self.increase_till(drink.price)
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -9,6 +9,13 @@ class TestPub(unittest.TestCase):
     def setUp(self):
         self.pub = Pub("The Prancing Pony", 100.00)
         self.pub1 = Pub("Bannermans", 200.00)
+        self.customer = Customer("Scott", 50.00, 20)
+        self.customer2 = Customer("Stuart", 20.00, 17)
+        self.customer3 = Customer("Stuart", 1.00, 18)
+        self.drink = Drink("Guinness", 1, 11)
+        self.drink2 = Drink("Guinness", 1, 5)
+        self.drink3 = Drink("Corona", 2, 5)
+        self.food = Food("Burger", 3.99, 3)
 
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
@@ -17,13 +24,11 @@ class TestPub(unittest.TestCase):
         self.assertEqual(100.00, self.pub.till)
 
     def test_age_legal(self):
-        customer = Customer("Scott", 50.00, 20)
-        age = self.pub.age_check(customer)
+        age = self.pub.age_check(self.customer)
         self.assertEqual(True, age)
 
     def test_age_not_legal(self):
-        customer = Customer("Scott", 50.00, 16)
-        age = self.pub.age_check(customer)
+        age = self.pub.age_check(self.customer2)
         self.assertEqual(False, age)
 
     def test_till_increase(self):
@@ -31,72 +36,68 @@ class TestPub(unittest.TestCase):
         self.assertEqual(103.00, self.pub.till)
 
     def test_drunkness_ok(self):
-        customer = Customer("Scott", 50.00, 20)
-        drunkness = self.pub.drunkness_check(customer)
+        drunkness = self.pub.drunkness_check(self.customer)
         self.assertEqual(True, drunkness)
 
     def test_drunkness__not_ok(self):
-        customer = Customer("Scott", 50.00, 20)
-        customer.increase_drunkness(20)
-        drunkness = self.pub.drunkness_check(customer)
+        self.customer.increase_drunkness(20)
+        drunkness = self.pub.drunkness_check(self.customer)
         self.assertEqual(False, drunkness)
 
     def test_sell_drink_ok(self):
-        customer = Customer("Scott", 50.00, 20)
-        drink = Drink("Guinness", 1, 11)
-        self.assertEqual(True, self.pub.age_check(customer))
-        self.assertEqual(True, self.pub.drunkness_check(customer))
-        self.pub.sell_drink(drink, customer)
-        self.assertEqual(49.00, customer.wallet)
+        self.assertEqual(True, self.pub.age_check(self.customer))
+        self.assertEqual(True, self.pub.drunkness_check(self.customer))
+        self.pub.sell_drink(self.drink, self.customer)
+        self.assertEqual(49.00, self.customer.wallet)
         self.assertEqual(101.00, self.pub.till)
-        self.assertEqual(11, customer.drunkness)
+        self.assertEqual(11, self.customer.drunkness)
 
     def test_sell_drink_underage(self):
-        customer = Customer("Stuart", 20.00, 17)
-        drink = Drink("Guinness", 1, 11)
-        self.assertEqual(False, self.pub.age_check(customer))
-        self.assertEqual(True, self.pub.drunkness_check(customer))
-        self.pub.sell_drink(drink, customer)
-        self.assertEqual(20.00, customer.wallet)
+        self.assertEqual(False, self.pub.age_check(self.customer2))
+        self.assertEqual(True, self.pub.drunkness_check(self.customer2))
+        self.pub.sell_drink(self.drink, self.customer2)
+        self.assertEqual(20.00, self.customer2.wallet)
         self.assertEqual(100.00, self.pub.till)
-        self.assertEqual(0, customer.drunkness)
+        self.assertEqual(0, self.customer2.drunkness)
 
     def test_sell_drink_too_drunk(self):
-        customer = Customer("Stuart", 20.00, 18)
-        drink = Drink("Guinness", 1, 11)
-        self.assertEqual(True, self.pub.age_check(customer))
-        self.assertEqual(True, self.pub.drunkness_check(customer))
-        self.pub.sell_drink(drink, customer)
-        self.assertEqual(19.00, customer.wallet)
-        self.assertEqual(101.00, self.pub.till)
-        self.assertEqual(11, customer.drunkness)
+        self.assertEqual(True, self.pub.age_check(self.customer))
+        self.assertEqual(True, self.pub.drunkness_check(self.customer))
 
-        self.assertEqual(True, self.pub.age_check(customer))
-        self.assertEqual(False, self.pub.drunkness_check(customer))
-        self.pub.sell_drink(drink, customer)
-        self.assertEqual(19.00, customer.wallet)
+        self.pub.sell_drink(self.drink, self.customer)
+        self.assertEqual(49.00, self.customer.wallet)
         self.assertEqual(101.00, self.pub.till)
-        self.assertEqual(11, customer.drunkness)
+        self.assertEqual(11, self.customer.drunkness)
+
+        self.assertEqual(True, self.pub.age_check(self.customer))
+        self.assertEqual(False, self.pub.drunkness_check(self.customer))
+        self.pub.sell_drink(self.drink, self.customer)
+        self.assertEqual(49.00, self.customer.wallet)
+        self.assertEqual(101.00, self.pub.till)
+        self.assertEqual(11, self.customer.drunkness)
 
     def test_sell_food(self):
-        customer = Customer("Scott", 50.00, 20)
-        food = Food("Burger", 3.99, 3)
-        self.pub.sell_food(food, customer)
-        self.assertEqual(46.01, customer.wallet)
+        self.pub.sell_food(self.food, self.customer)
+        self.assertEqual(46.01, self.customer.wallet)
         self.assertEqual(103.99, self.pub.till)
-        self.assertEqual(-3, customer.drunkness)
+        self.assertEqual(-3, self.customer.drunkness)
 
     def test_quantity_check(self):
         drink = self.pub.check_quantity("Guinness")
         self.assertEqual(5, self.pub.check_quantity("Guinness"))
 
     def test_reduce_quantity(self):
-        drink = Drink("Guinness", 1, 11)
         self.pub.reduce_quantity("Guinness")
-        self.assertEqual(4, self.pub.check_quantity(drink.name))
+        self.assertEqual(4, self.pub.check_quantity(self.drink.name))
 
     def test_stock_value(self):
-        drink = Drink("Guinness", 1, 5)
-        drink2 = Drink("Corona", 2, 5)
-        drink_list = [drink, drink2]
+        drink_list = [self.drink2, self.drink3]
         self.assertEqual(7.00, self.pub.stock_value(drink_list))
+
+    def test_sufficient_funds(self):
+        fund_check = self.pub.sufficient_funds(self.customer, self.drink3)
+        self.assertEqual(True, fund_check)
+
+    def test_insufficient_funds(self):
+        fund_check = self.pub.sufficient_funds(self.customer3, self.drink3)
+        self.assertEqual(False, fund_check)
